fe01cfde4a0d44024eaea9209b63ed15a4183415
Diego ROMERO RODRIGUEZ <diego.romerorodriguez@mirakl.com>
2023-12-31 00:40:02 +0100

    Add property 'cards' required in 'Column' type

--- src/components/__tests__/DeleteColumn.spec.ts
+++ src/components/__tests__/DeleteColumn.spec.ts
@@ -6,34 +6,34 @@ describe('delete one column in our array of columns', () => {
   it.each([
     {
       columns: [
-        { id: 1, title: 'Column 1' },
-        { id: 2, title: 'Column 2' }
+        { id: 1, title: 'Column 1', cards: [] },
+        { id: 2, title: 'Column 2', cards: [] }
       ],
       idToDelete: 2,
-      expectedFilteredColumns: [{ id: 1, title: 'Column 1' }]
+      expectedFilteredColumns: [{ id: 1, title: 'Column 1', cards: [] }]
     },
     {
       columns: [
-        { id: 1, title: 'Column 1' },
-        { id: 2, title: 'Column 2' }
+        { id: 1, title: 'Column 1', cards: [] },
+        { id: 2, title: 'Column 2', cards: [] }
       ],
       idToDelete: 3,
       expectedFilteredColumns: [
-        { id: 1, title: 'Column 1' },
-        { id: 2, title: 'Column 2' }
+        { id: 1, title: 'Column 1', cards: [] },
+        { id: 2, title: 'Column 2', cards: [] }
       ]
     },
     {
       columns: [
-        { id: 1, title: 'Column 1' },
-        { id: 2, title: 'Column 2' },
-        { id: 2, title: 'Column 2' },
-        { id: 3, title: 'Column 3' }
+        { id: 1, title: 'Column 1', cards: [] },
+        { id: 2, title: 'Column 2', cards: [] },
+        { id: 2, title: 'Column 2', cards: [] },
+        { id: 3, title: 'Column 3', cards: [] }
       ],
       idToDelete: 2,
       expectedFilteredColumns: [
-        { id: 1, title: 'Column 1' },
-        { id: 3, title: 'Column 3' }
+        { id: 1, title: 'Column 1', cards: [] },
+        { id: 3, title: 'Column 3', cards: [] }
       ]
     },
     {
