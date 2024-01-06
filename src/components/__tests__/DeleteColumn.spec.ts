3490cbe2bd2b031639d68a0099efb71a651f023f
Diego ROMERO RODRIGUEZ <diego.romerorodriguez@mirakl.com>
2024-01-06 23:35:43 +0100

    Use ULIDs instead of random numbers to generate ids

--- src/components/__tests__/DeleteColumn.spec.ts
+++ src/components/__tests__/DeleteColumn.spec.ts
@@ -40,6 +40,16 @@ describe('delete one column in our array of columns', () => {
       columns: [],
       idToDelete: 2,
       expectedFilteredColumns: []
+    },
+    {
+      columns: [
+        { id: '42e8906a-4277-4845-a83d-e0b861858775', title: 'Column 1', cards: [] },
+        { id: 'b34872d7-af57-4e5c-9be1-25cc56a46ac8', title: 'Column 2', cards: [] }
+      ],
+      idToDelete: 'b34872d7-af57-4e5c-9be1-25cc56a46ac8',
+      expectedFilteredColumns: [
+        { id: '42e8906a-4277-4845-a83d-e0b861858775', title: 'Column 1', cards: [] },
+      ]
     }
   ])(
     'filterColumn("$columns, $idToDelete") -> $expectedFilteredColumns',
