1b4e686be2b6e05f32d609cc4e99d18023aa77a8
Marcos S <45607262+MarcosNASA@users.noreply.github.com>
2024-01-10 00:45:01 +0100

    Revert "fix: modal on click outside (#7)" (#9)
    This reverts commit 92d0ff6f1acd58074b941f90e6fe58aa3d6f28a7.

--- src/components/CardDetail.tsx
+++ src/components/CardDetail.tsx
@@ -1,7 +1,6 @@
 import { X } from 'lucide-react'
 import { Card, Id } from '../types'
 import { useColumns } from './store/useColumns'
-import { useEffect, useRef } from 'react'
 
 type Props = {
   closeDetails: () => void
@@ -31,63 +30,45 @@ export const CardDetail = (props: Props) => {
     addCardImage('', id, props.columnId)
   }
 
-  const modalRef = useRef<HTMLDivElement | null>(null)
-
-  useEffect(() => {
-    const handleClickOutside = (event: any) => {
-      if (!modalRef.current) return
-      if (modalRef.current.contains(event.target)) return
-      props.closeDetails()
-    }
-
-    document.addEventListener('click', handleClickOutside)
-    return () => {
-      document.removeEventListener('click', handleClickOutside)
-    }
-  }, [])
-
   return (
     <div
-      id="card-container"
-      className="fixed w-screen h-screen flex justify-center items-center inset-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      id='card-container'
+      className='fixed w-screen h-screen flex justify-center items-center inset-1/2 transform -translate-x-1/2 -translate-y-1/2'
     >
-      <div
-        ref={modalRef}
-        className="w-fit min-w-[500px] flex flex-col gap-y-4 p-4 bg-mainBackgroundColor border-2 border-columnBackgroundColor"
-      >
+      <div className='w-fit min-w-[500px] flex flex-col gap-y-4 p-4 bg-mainBackgroundColor border-2 border-columnBackgroundColor'>
         <X
           onClick={() => {
             props.closeDetails()
           }}
-          className="text-customWhite"
+          className='text-customWhite'
         />
 
-        <label className="w-full flex items-center">
+        <label className='w-full flex items-center'>
           <input
             value={title}
-            id="input-name"
-            onChange={(e) => handleTitleChange(e.target.value)}
-            className="bg-transparent rounded-[4px] mr-2 px-2 pt-2 flex-grow"
+            id='input-name'
+            onChange={e => handleTitleChange(e.target.value)}
+            className='bg-transparent rounded-[4px] mr-2 px-2 pt-2 flex-grow'
           />
         </label>
 
         <label>
           <textarea
             value={description}
-            onChange={(e) => handleDescriptionChange(e.target.value)}
-            className="bg-[#1E2733] p-4 rounded-[4px] text-customWhite h-[100px] w-full"
+            onChange={e => handleDescriptionChange(e.target.value)}
+            className='bg-[#1E2733] p-4 rounded-[4px] text-customWhite h-[100px] w-full'
           />
         </label>
 
         <div>
           <input
-            type="checkbox"
+            type='checkbox'
             checked={imageCovered}
-            id="cover"
-            name="cover"
+            id='cover'
+            name='cover'
             onChange={handleImgIsCover}
           />
-          <label htmlFor="cover" className="ml-2">
+          <label htmlFor='cover' className='ml-2'>
             Put image as cover
           </label>
           {srcImage && (
@@ -96,14 +77,14 @@ export const CardDetail = (props: Props) => {
                 onClick={() => {
                   deleteImage()
                 }}
-                className="absolute top-0 left-0 right-0 z-20 bg-[#1E2733] rounded-[4px] py-2 hover:bg-slate-700"
+                className='absolute top-0 left-0 right-0 z-20 bg-[#1E2733] rounded-[4px] py-2 hover:bg-slate-700'
               >
                 Delete
               </button>
               <img
                 src={srcImage}
                 alt={`img-${title}`}
-                className="max-w-[200px] mt-4"
+                className='max-w-[200px] mt-4'
               />
             </>
           )}
