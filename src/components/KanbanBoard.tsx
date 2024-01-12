42578470621d1c3c28b64880d3e79ea1bfebefa8
Jarrison Cano <jarrison.cano@hotmail.com>
2024-01-12 12:00:20 -0500

    remove vertical scroll from the homepage

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -74,48 +74,47 @@ export const KanbanBoard = () => {
   )
 
   return (
-    <div>
+    <div
+      className='mx-auto flex flex-1 w-full items-center overflow-x-auto px-[40px]'>
       <DndContext
         sensors={sensors}
         onDragStart={onDragStart}
         onDragEnd={onDragEnd}
       >
-        <div className='m-auto flex min-h-screen w-full mt-8 overflow-x-auto overflow-y-hidden px-[40px]'>
-          <div className='flex'>
-            <SortableContext
-              items={columnsId}
-              strategy={horizontalListSortingStrategy}
-            >
-              <div className='flex gap-x-4 h-[300px] h-fit'>
-                {columns.map(column => (
-                  <ColumnContainer
-                    key={column.id}
-                    column={column}
-                    deleteColumn={deleteColumn}
-                  />
-                ))}
-              </div>
-              <button
-                onClick={createNewColumn}
-                className='h-[60px] w-[350px] flex items-center justify-center gap-2 min-w-[350px] cursor-pointer ml-4 rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor'
-              >
-                <PlusCircle />
-                Add column
-              </button>
-            </SortableContext>
-          </div>
-          {createPortal(
-            <DragOverlay>
-              {activeColumn && (
+        <div className='mx-auto flex'>
+          <SortableContext
+            items={columnsId}
+            strategy={horizontalListSortingStrategy}
+          >
+            <div className='flex gap-x-4 h-[300px] h-fit'>
+              {columns.map(column => (
                 <ColumnContainer
-                  column={activeColumn}
+                  key={column.id}
+                  column={column}
                   deleteColumn={deleteColumn}
                 />
-              )}
-            </DragOverlay>,
-            document.body
-          )}
+              ))}
+            </div>
+            <button
+              onClick={createNewColumn}
+              className='h-[60px] w-[350px] flex items-center justify-center gap-2 min-w-[350px] cursor-pointer ml-4 rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor'
+            >
+              <PlusCircle />
+              Add column
+            </button>
+          </SortableContext>
         </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={deleteColumn}
+              />
+            )}
+          </DragOverlay>,
+          document.body
+        )}
       </DndContext>
     </div>
   )
