19159b5599da6fc72c104652495cef1ce434d690
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-03-09 16:51:11 -0400

    chore: remove all node project file

--- src/utils/createColumn.ts
+++
@@ -1,9 +0,0 @@
-import { generateId } from './generateId'
-import {Column} from "../types";
-
-export const createColumn = (columnNumber: number) : Column => {
-  return {
-    id: generateId(),
-    title: `Column ${columnNumber}`,
-  }
-}
--- src/utils/filterColumn.ts
+++
@@ -1,5 +0,0 @@
-import { Column, Id } from '../types'
-
-export const filterColumn = (columns: Column[], id: Id) => {
-  return columns.filter(col => col.id !== id)
-}
